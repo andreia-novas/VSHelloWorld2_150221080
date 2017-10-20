769b9a0bca3ff95661af8de6f0f98257a0f66f26
andreia-novas <andreianovas93@gmail.com>
2017-10-20 17:13:58 +0100

    Adicionado Comentario

--- VSHelloWorld2_150221080/Program.cs
+++ VSHelloWorld2_150221080/Program.cs
@@ -11,6 +11,7 @@ namespace VSHelloWorld2_150221080
     {
         public static void Main(string[] args)
         {
+            //Comentário
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
